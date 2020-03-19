cc89a03d5ac03af18e441fd2eb2e00814b6832fe
Daniel Prue <42305970+fireblastdaniel@users.noreply.github.com>
2020-03-19 01:00:45 -0700

    helper functions in scheme-model built out

---
+++ schemes/scheme-model.js
@@ -0,0 +1,49 @@
+const db = require('../data/db-config.js');
+
+module.exports = {
+  find,
+  findById,
+  findSteps,
+  add,
+  update,
+  remove
+};
+
+function find() {
+  return db('schemes');
+}
+
+function findById(id) {
+  return db('schemes')
+    .where({ id })
+    .first();
+}
+
+function findSteps(id) {
+  return db('steps')
+    .where('scheme_id', id)
+    .orderBy('step_number');
+}
+
+function add(scheme) {
+  return db('schemes')
+    .insert(scheme, 'id')
+    .then(ids => {
+      return findById(ids[0]);
+    });
+}
+
+function update(changes, id) {
+  return db('schemes')
+    .where({ id })
+    .update(changes)
+    .then(() => {
+      return findById(id);
+    });
+}
+
+function remove(id) {
+  return db('schemes')
+    .where({ id })
+    .del();
+}
